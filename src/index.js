690e6a1b316a2fe47a08c2b59d4878171164403f
Hayeon-Lee <papepopepe@naver.com>
2025-05-18 19:21:23 +0900

    연산자

--- src/index.js
+++ src/index.js
@@ -1,43 +1,70 @@
-let integer = 10;
-console.log(typeof integer);
+//산술 연산자
 
-let float = 49.99;
-console.log(typeof float);
-
-let inf = 100 / 0;
-console.log(inf); //출력: Infinity
-console.log(typeof inf);
-
-let nan = "string" / 10;
-console.log(nan); //출력: NaN
-console.log(typeof nan);
-
-let bigNum1 = 999999999999999n;
-let bigNum2 = BigInt("9999999999999999");
-
-console.log(typeof bigNum1);
-console.log(typeof bigNum2);
-
-let username = "hayeon";
-let intro = `내 이름은 ${username} 입니다.`;
-console.log(intro);
-
-let isClicked = false;
-if (isClicked) console.log("클릭.");
-else console.log("클릭 안 됨.");
-
-let empty = null;
-console.log(typeof empty); //오류: object 가 출력됨
-console.log(empty === null); //true로 출력됨으로써, null 이 맞음.
-
-let undefined_num;
-console.log(typeof undefined_num);
-
-let num1 = "15";
+let num1 = 10;
 let num2 = 5;
-console.log(num1 / num2); //3
 
-let num3 = "15";
-let num4 = 5;
-console.log(num3 + num4); //155
-console.log(parseInt(num3, 10) + num4);
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 / num2);
+console.log(num1 * num2);
+console.log(num1 % num2);
+
+console.log(num1++);
+console.log(++num1);
+
+let num3 = 15;
+num3 += 5;
+num3 -= 5;
+num3 /= 5;
+num3 *= 5;
+num3 %= 5;
+
+let num4 = 10;
+let num5 = "10";
+
+console.log(num4 === num5); //false
+console.log(num4 == num5); //true
+
+console.log(num4 != num5); //false
+console.log(num4 !== num5); //true
+
+let a = 10;
+let b = 20;
+let c = 10;
+
+console.log(a < b);
+console.log(a > b);
+console.log(b >= c);
+console.log(b > c);
+console.log(a <= c);
+console.log(a > c);
+
+let price = 10000;
+console.log("가격: " + price + "원");
+
+let isClicked = true;
+let isOpened = false;
+
+console.log(!isClicked);
+console.log(!isOpened);
+
+console.log(true || true); //true
+console.log(true || false); //true
+console.log(false || true); //true
+console.log(false || false); //false
+
+console.log(true && true); //true
+console.log(true && false); //false
+console.log(false && true); //false
+console.log(false && false); //false
+
+let num6;
+let num7 = 10;
+
+console.log(num6 ?? 20); //20
+console.log(num7 ?? 20); //10
+
+let num = 100;
+let result = num % 2 === 0 ? "짝수" : "홀수";
+
+console.log(result);
