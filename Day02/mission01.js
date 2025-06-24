11d8da0649a47532fd30a2da3f3acc117c37c520
Hayeon-Lee <papepopepe@naver.com>
2025-06-25 02:05:02 +0900

    JS 한 입 챌린지 - Day02

---
+++ Day02/mission01.js
@@ -0,0 +1,18 @@
+let animal = "cat";
+
+switch (animal) {
+  case "dog":
+    console.log("개");
+    break;
+  case "tiger":
+    console.log("호랑이");
+    break;
+  case "cat":
+    console.log("고양이");
+    break;
+  case "lion":
+    console.log("사자");
+    break;
+  default:
+    console.log("동물 출력하기");
+}
